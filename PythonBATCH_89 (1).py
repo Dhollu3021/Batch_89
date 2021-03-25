348731789b2d413e225729b25e25f96745b3cb81
Dhollu3021 <81221534+Dhollu3021@users.noreply.github.com>
2021-03-25 12:30:08 +0530

    Add files via upload

---
+++ PythonBATCH_89 (1).py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+print("hello Aashavi ")
+
+
+# In[2]:
+
+
+print("hello jyoti")
+
+
+# In[1]:
+
+
+# Variables Declarations 
+
+
+# In[5]:
+
+
+# Varaiabe shout not start with the special characters or number or space
+_vaibhav='name'  # valid Variable name as discussed in the class # _ can we used while declarations the variable name
+1vaibhav='name'  # invalid Variabe name # number cann't we used in the starting of the variable name
+@vaibhav='name'  # invalid Variable name #special Characters are not allowed in the declaration of the varaible name
+
+
+# In[7]:
+
+
+print(_vaibhav)
+
+
+# In[8]:
+
+
+print(1vaibhav)
+
+
+# In[9]:
+
+
+print(@vaibhav)
+
+
+# In[18]:
+
+
+# Data Types In python 
+# Str
+# Number (int , float )
+# list
+# tuples
+name='vaibhav_sharma'
+xyz=3
+xd=5.0
+
+
+# In[11]:
+
+
+print(name)
+
+
+# In[12]:
+
+
+print(name.title())
+
+
+# In[16]:
+
+
+print(name.upper())
+
+
+# In[19]:
+
+
+print(xyz)
+
+
+# In[20]:
+
+
+print(xd)
+
+
+# In[24]:
+
+
+print(type(xyz))
+
+
+# In[25]:
+
+
+print(type(xd))
+
+
+# In[26]:
+
+
+print(type(name))
+
+
+# In[ ]:
+
+
+
+
